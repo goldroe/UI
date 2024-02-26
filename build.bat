4fec5a9fbcd8f473dad425fc91f88c6d15b340c0
Carlos Rivera <cr4185320@gmail.com>
2024-02-26 00:49:09 -0600

    Ignore unused var warning

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ SET includes=/Iext\freetype\include
 SET sources=src\ui_demo.cpp src\UI.cpp
 SET libs=freetype.lib user32.lib kernel32.lib winmm.lib
 
-SET warning_flags=/W4 /wd4100 /wd4189 /wd4530 /wd4201
+SET warning_flags=/W4 /wd4100 /wd4189 /wd4530 /wd4201 /wd4101
 SET compiler_flags=/nologo /FC %warning_flags% %includes% /Fdbuild\ /Fobuild\
 SET linker_flags=/OPT:REF -INCREMENTAL:NO /debug /IGNORE:4098 /IGNORE:4099 /LIBPATH:ext\freetype\ %libs% 
 
